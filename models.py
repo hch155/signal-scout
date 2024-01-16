d9b84958d0e73761d847cb111b16a38b035793db
hch155 <hcylwik@gmail.com>
2024-01-16 21:11:24 +0100

    Fixed import issues, enhanced base station grouping and data aggregation

--- models.py
+++ models.py
@@ -3,10 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class BaseStation(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    IdStacji = db.Column(db.String, nullable=False)  # Real base station ID
-    Miejscowość = db.Column(db.String, nullable=False)  # City
-    Lokalizacja = db.Column(db.String, nullable=False)  # Street number
+    id = db.Column(db.Integer, primary_key=True)            # db index 
+    basestation_id = db.Column(db.String, nullable=True)    # Real base station ID
+    city = db.Column(db.String, nullable=True)              # City
+    location = db.Column(db.String, nullable=True)          # Location/Street number
+    service_provider = db.Column(db.String, nullable=True)  # ISP
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
     frequency_band = db.Column(db.String, nullable=False)
