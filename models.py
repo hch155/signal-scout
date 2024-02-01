398e6d842c47dcc150745430267df5b9af7966d6
hch155 <hcylwik@gmail.com>
2024-02-01 13:04:34 +0100

    Improved sorting bands in Stats page by using fixed order

--- models.py
+++ models.py
@@ -11,6 +11,3 @@ class BaseStation(db.Model):
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
     frequency_band = db.Column(db.String, nullable=False)
-    # Add other fields as necessary, for example:
-    # provider_name = db.Column(db.String, nullable=True)
-    # signal_strength = db.Column(db.Float, nullable=True)
