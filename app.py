a846cd6224c9d607d801053acf83d6ab7d6ff566
hch155 <hcylwik@gmail.com>
2024-02-28 09:08:03 +0100

    Refine filtering logic and enhance UI interactivity
    This commit introduces several key enhancements to the application's filtering functionality and user interface, notably: separation of filtering per quantity of BTSs', BTSs' within distance, service providers and frequency bands

--- app.py
+++ app.py
@@ -98,11 +98,14 @@ def get_stations():
         max_distance = request.args.get('max_distance', default=None, type=float)
         limit = request.args.get('limit', default=6, type=int)
         frequency_bands = request.args.getlist('frequency_bands')
-        selected_service_provider = request.args.get('service_provider')
-        if selected_service_provider is not None:
-            selected_service_provider = selected_service_provider.rstrip("'")
-        else:
-            selected_service_provider = None
+        selected_service_provider = request.args.getlist('service_provider')
+        selected_service_provider= []
+        for provider in selected_service_provider:
+            if provider == "P4 Sp. z o.o.'":
+                provider = provider.rstrip("'")
+                selected_service_provider.append(provider)        
+            else:
+                selected_service_provider = None
 
         result = find_nearest_stations(user_lat, user_lng, max_distance=max_distance, limit=limit)
         stations = result.get("stations", [])
