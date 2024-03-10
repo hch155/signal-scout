b1bfa64725dfa7dceec4b09f09d27a5ff2852476
hch155 <hcylwik@gmail.com>
2024-03-10 19:47:28 +0100

    Refactor: Organize UI interaction code to functions, enhancement of user states

--- static/scripts/pages/common.js
+++ static/scripts/pages/common.js
@@ -1,5 +1,16 @@
 // base.html
 
+document.addEventListener('DOMContentLoaded', function() {
+  initializeModalToggle();
+  initializeFormSubmissions();
+  adjustFooterPosition();
+  initializeThemeToggle();
+  conditionalCheckLoginState();
+  initializeScrollToTop();
+});
+
+window.addEventListener('resize', adjustFooterPosition);
+
 function adjustFooterPosition() {
     const footer = document.querySelector('footer'); 
     const bodyHeight = document.body.offsetHeight;
@@ -12,13 +23,98 @@ function adjustFooterPosition() {
    }
 }
 
-function logoutUser() {
-  fetch('/logout', { method: 'POST' })
-  .then(response => {
-      if (response.ok) {
-          alert('Logged out successfully.');
-      } else {
-          alert('Logout failed. Please try again.');
+function initializeThemeToggle() {
+  const storedTheme = localStorage.getItem('theme');
+  const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const isDarkModePreferred = storedTheme === 'dark' || (!storedTheme && prefersDarkMode);
+
+  document.documentElement.classList.toggle('dark', isDarkModePreferred);
+
+  const btnThemeToggler = document.getElementById('themeToggle');
+  if (btnThemeToggler) {
+      btnThemeToggler.textContent = isDarkModePreferred ? 'Light Mode' : 'Dark Mode';
+
+      btnThemeToggler.addEventListener('click', () => {
+          const isDarkModeNow = document.documentElement.classList.toggle('dark');
+          localStorage.setItem('theme', isDarkModeNow ? 'dark' : 'light');
+          btnThemeToggler.textContent = isDarkModeNow ? 'Light Mode' : 'Dark Mode';
+
+          // Custom event indicating the theme has changed
+          window.dispatchEvent(new CustomEvent('themeChanged', { detail: { isDarkMode: isDarkModeNow } }));
+      });
+  }
+}
+
+// Scroll up
+function initializeScrollToTop() {
+  const backToTopBtn = document.getElementById('backToTopBtn');
+
+  if (backToTopBtn) {
+    window.addEventListener('scroll', () => {
+        backToTopBtn.classList.toggle('hidden', window.scrollY <= 200);
+    });
+
+    backToTopBtn.addEventListener('click', () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+  }
+}
+
+function initializeModalToggle() {
+  const registerBtn = document.getElementById('registerBtn');
+  const signInBtn = document.getElementById('signInBtn');
+  const registrationModal = document.getElementById('registrationModal');
+  const signInModal = document.getElementById('signInModal');
+
+  registerBtn.addEventListener('click', () => registrationModal.classList.toggle('hidden'));
+  signInBtn.addEventListener('click', () => signInModal.classList.toggle('hidden'));
+
+  document.querySelectorAll('.close-modal').forEach(button => {
+      button.addEventListener('click', () => button.closest('.modal').classList.add('hidden'));
+  });
+
+  [registrationModal, signInModal].forEach(modal => {
+      modal.addEventListener('click', (event) => {
+          if (event.target === modal) modal.classList.add('hidden');
+      });
+  });
+}
+
+function initializeFormSubmissions() {
+  document.getElementById('registrationForm').addEventListener('submit', handleRegistrationSubmit);
+  document.getElementById('signInForm').addEventListener('submit', handleSignInSubmit);
+}
+
+function handleRegistrationSubmit(e) {
+  e.preventDefault(); // Prevent default form submission
+  const formData = new FormData(e.target);
+  const password = formData.get('password');
+  const confirmPassword = formData.get('confirm_password');
+
+  if (password !== confirmPassword) {
+      alert('Passwords do not match.');
+      return;
+  }
+
+  submitForm('/register', formData);
+}
+
+function handleSignInSubmit(e) {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  submitForm('/login', formData);
+}
+
+function submitForm(url, formData) {
+  fetch(url, { method: 'POST', body: formData })
+  .then(response => response.json())
+  .then(data => {
+      alert(data.message);
+      if (data.success) {
+          checkLoginStateAndUpdateUI();
+          if (url === '/login') {
+              localStorage.setItem('loggedIn', 'true');
+          }
       }
   })
   .catch(error => {
@@ -27,161 +123,78 @@ function logoutUser() {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    adjustFooterPosition();
-});
-
-window.addEventListener('resize', adjustFooterPosition);
-
-document.addEventListener('DOMContentLoaded', () => {
-    const storedTheme = localStorage.getItem('theme');
-    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    const isDarkModePreferred = storedTheme === 'dark' || (!storedTheme && prefersDarkMode);
-
-    document.documentElement.classList.toggle('dark', isDarkModePreferred);
-
-    const btnThemeToggler = document.getElementById('themeToggle');
-    if (btnThemeToggler) {
-        btnThemeToggler.textContent = isDarkModePreferred ? 'Light Mode' : 'Dark Mode';
-        
-        btnThemeToggler.addEventListener('click', () => {
-            const isDarkModeNow = document.documentElement.classList.toggle('dark');
-            localStorage.setItem('theme', isDarkModeNow ? 'dark' : 'light');
-            btnThemeToggler.textContent = isDarkModeNow ? 'Light Mode' : 'Dark Mode';
-
-            // Custom event indicating the theme has changed
-            window.dispatchEvent(new CustomEvent('themeChanged', { detail: { isDarkMode: isDarkModeNow } }));
-        });
-    }
-});
-
-// Scroll up
-document.addEventListener('DOMContentLoaded', (event) => {
-    const backToTopBtn = document.getElementById('backToTopBtn');
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > 200) {
-            backtoTopBtn.classList.remove('hidden');
-        } else {
-            backtoTopBtn.classList.add('hidden');
-        }
-    backtoTopBtn.addEventListener('click', () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-    });
- });
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-    const registerBtn = document.getElementById('registerBtn');
-    const signInBtn = document.getElementById('signInBtn');
-    const registrationModal = document.getElementById('registrationModal');
-    const signInModal = document.getElementById('signInModal');
-  
-    registerBtn.addEventListener('click', () => {
-      registrationModal.classList.toggle('hidden');
-    });
-  
-    signInBtn.addEventListener('click', () => {
-      signInModal.classList.toggle('hidden');
-    });
-  
-    document.querySelectorAll('.close-modal').forEach(button => {
-      button.addEventListener('click', () => {
-        const modalToClose = button.closest('.modal'); // common class for all modals
-        if (modalToClose) {
-          modalToClose.classList.add('hidden');
-        }
-      });
-    });
-  
-    registrationModal.addEventListener('click', (event) => {
-      if (event.target === registrationModal) {
-        registrationModal.classList.add('hidden');
+function logoutUser() {
+  fetch('/logout', { method: 'POST' })
+  .then(response => response.json())
+  .then(data => {
+      if (data.success) {
+          localStorage.removeItem('loggedIn');
+          resetUIAndListeners();
+      } else {
+          console.error('Logout failed:', data.message);
       }
-    });
-  
-    signInModal.addEventListener('click', (event) => {
-      if (event.target === signInModal) {
-        signInModal.classList.add('hidden');
-      }
-    });
-  });
+  })
+  .catch(error => console.error('Error during logout:', error));
+}
 
-  document.getElementById('registrationForm').addEventListener('submit', function(e) {
-    e.preventDefault(); // Prevent default form submission
-  
-    const formData = new FormData(this); // 'this' refers to the form
-    const password = formData.get('password');
-    const confirmPassword = formData.get('confirm_password');
+function safelyUpdateDisplay(elementId, displayStyle) {
+  const element = document.getElementById(elementId);
+  if (element) {
+    element.style.display = displayStyle;
+  }
+}
 
-    if (password !== confirmPassword) {
-      alert('Passwords do not match.');
-      return;
-    }
-  
-    fetch('/register', { 
-      method: 'POST',
-      body: formData
-    })
+function checkLoginStateAndUpdateUI() {
+  fetch('/session_check')
     .then(response => response.json())
     .then(data => {
-      if (data.success) {
-        alert('Registration successful!');
+      if (data.logged_in) {
+        safelyUpdateDisplay('registrationModal', 'none');
+        safelyUpdateDisplay('signInModal', 'none');
+        safelyUpdateDisplay('signInBtn', 'none');
+        safelyUpdateDisplay('registerBtn', 'none');
+        safelyUpdateDisplay('logoutButton', 'block');
       } else {
-        alert('Registration failed: ' + data.error);
+        safelyUpdateDisplay('registrationModal', 'none');
+        safelyUpdateDisplay('signInModal', 'none');
+        safelyUpdateDisplay('signInBtn', 'block');
+        safelyUpdateDisplay('registerBtn', 'block');
+        safelyUpdateDisplay('logoutButton', 'none');
       }
     })
-    .catch(error => {
-      console.error('Error:', error);
-      alert('An error occurred. Please try again.');
-    });
-  });
+    .catch(error => console.error('Error checking login state:', error));
+}
 
-  document.getElementById('signInForm').addEventListener('submit', function(e) {
-    e.preventDefault();
+function conditionalCheckLoginState() {
+  const mightBeLoggedIn = localStorage.getItem('loggedIn') === 'true';
 
-    const formData = new FormData(this);
+  if (mightBeLoggedIn) {
+    checkLoginStateAndUpdateUI();
+  } else {
+    adjustUIForLoggedOutState();
+  }
+}
 
-    fetch('/login', { 
-        method: 'POST',
-        body: new FormData(document.getElementById('signInForm'))
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        if (data.success) {
-            alert(data.message);
-        } else {
-            alert(data.message);
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('An error occurred during login. Please try again.');
-    });
-});
+function adjustUIForLoggedOutState() {
+  document.getElementById('registrationModal').classList.add('hidden');
+  document.getElementById('signInModal').classList.add('hidden');
+  document.getElementById('signInBtn').style.display = 'block'; 
+  document.getElementById('registerBtn').style.display = 'block'; 
+  document.getElementById('logoutButton').style.display = 'none';
+}
 
-document.addEventListener('DOMContentLoaded', (event) => {
-  fetch('/session_check')
-      .then(response => response.json())
-      .then(data => {
-          if (data.logged_in) {
-              document.getElementById('signInForm').style.display = 'none'; // hide
-              document.getElementById('registrationForm').style.display = 'none';
-              document.getElementById('logoutButton').style.display = 'block';
+function resetUIAndListeners() {
+  adjustUIForLoggedOutState(); 
+  initializeModalToggle();
+  initializeFormSubmissions();
+}
 
-          } else {
-              document.getElementById('signInForm').style.display = 'block';
-              document.getElementById('registrationForm').style.display = 'block'
-              document.getElementById('logoutButton').style.display = 'none';
-          }
-      })
-      .catch(error => console.error('Error checking session state:', error));
-});
+function debugSession() {
+  fetch('/debug_session')
+  .then(response => response.json())
+  .then(data => {
+      console.log('Debug Session:', data);
+  })
+  .catch(error => console.error('Error debugging session:', error));
+}
 
