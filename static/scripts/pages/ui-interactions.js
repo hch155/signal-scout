f9f630e56d15698ec016ea3682dfe1c8f0f7fe9e
hch155 <hcylwik@gmail.com>
2024-03-17 14:28:45 +0100

    Improve UI: Align lat. lng inputs in filter-container for neatness

--- static/scripts/pages/ui-interactions.js
+++ static/scripts/pages/ui-interactions.js
@@ -144,7 +144,7 @@ filterControl.onAdd = function(map) {
 
         <div id="filterContainer" class="bg-white p-1 rounded shadow text-black dark:bg-black dark:text-white w-76 accent-blue-500 dark:accent-gray-400 hidden">
             <div class="static-content">
-                <div class="my-2>
+                <div class="my-2">
                     <p class="text-gray-700 font-bold dark:text-white">Service Provider:</p>
                     <div class="flex flex-wrap label-container gap-1">    
                         <label><input type="checkbox" name="service_provider" value="P4 Sp. z o.o.'"> Play</label>
@@ -188,7 +188,7 @@ filterControl.onAdd = function(map) {
                     <input type="number" id="withinDistanceRange" min="0.0" max="10" step="0.1" placeholder="" class="w-[4.25rem] mt-1 bg-blue-100 hover:bg-blue-300 dark:bg-gray-700 dark:hover:bg-gray-500">
                 </div>
 
-                <div id="dynamicContent"
+                <div id="dynamicContent">
                 
                 </div>
             </div> 
@@ -596,10 +596,14 @@ function updateDynamicContent() {
             dynamicContent.innerHTML = `
                 <div id="latLngContainer" class="opacity-50 cursor-not-allowed flex flex-col space-y-0.5">
                     <button id="submitCoords" class="w-48 bg-blue-300 dark:bg-gray-700 dark:bg-gray-700 hover:bg-blue-500 dark:hover:bg-gray-500 text-white rounded cursor-not-allowed" disabled title="Register or log in to use this feature.">Search</button>
-                    <input type="number" id="latitudeInput" placeholder="52.230 (°N)" class="w-[5.5rem] bg-blue-100 hover:bg-blue-300 dark:bg-gray-700 dark:hover:bg-gray-500 cursor-not-allowed" min="48" max="58" step="0.1" disabled title="Register or log in to use this feature.">
-                    <input type="number" id="longitudeInput" placeholder="21.003 (°E)" class="w-[5.5rem] mt-1 bg-blue-100 hover:bg-blue-300 dark:bg-gray-700 dark:hover:bg-gray-500 cursor-not-allowed" min="13.5" max="24.5" step="0.1" disabled title="Register or log in to use this feature.">
+                    <div class="flex space-x-2">
+                        <input type="number" id="latitudeInput" placeholder="52.230 (°N)" class="w-[5.5rem] bg-blue-100 hover:bg-blue-300 dark:bg-gray-700 dark:hover:bg-gray-500 cursor-not-allowed" min="48" max="58" step="0.1" disabled title="Register or log in to use this feature.">
+                        <input type="number" id="longitudeInput" placeholder="21.003 (°E)" class="w-[5.5rem] bg-blue-100 hover:bg-blue-300 dark:bg-gray-700 dark:hover:bg-gray-500 cursor-not-allowed" min="13.5" max="24.5" step="0.1" disabled title="Register or log in to use this feature.">
+                    </div>
                     <button id="submitfilteredstation" class="w-48 mt-1 bg-blue-300 dark:bg-gray-700 hover:bg-blue-500 dark:hover:bg-gray-500 text-white rounded cursor-not-allowed" disabled title="Register or log in to use this feature.">Search</button>
-                    <input type="text" id="baseStationIdInput" placeholder="Enter Base Station ID" class="w-[8rem] mt-1 bg-blue-100 hover:bg-blue-300 dark:bg-gray-700 dark:hover:bg-gray-500 cursor-not-allowed" disabled title="Register or log in to use this feature.">
+                    <div class="flex space-x-2">
+                        <input type="text" id="baseStationIdInput" placeholder="Enter Base Station ID" class="w-[8rem] mt-1 bg-blue-100 hover:bg-blue-300 dark:bg-gray-700 dark:hover:bg-gray-500 cursor-not-allowed" disabled title="Register or log in to use this feature.">
+                    </div>
                 </div>`;
             }
     })
