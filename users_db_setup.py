71f3c9f91747a822e22b0b0ede73b8357d344950
hch155 <hcylwik@gmail.com>
2024-03-17 17:32:00 +0100

    Refactor: Separate creation of stations.db and users.db, code cleanup

--- users_db_setup.py
+++ users_db_setup.py
@@ -1,13 +1,14 @@
 from app import app
-from models import db, User
+from models import User
+from database import db
 
 with app.app_context():
     db.create_all()
 
 with app.app_context():
     if not User.query.filter_by(email='test@example.com').first():
-        test_user = User(email='userinitdbtest@example.com')
-        test_user.set_password('testpassword') 
+        test_user = User(email='initdbtest@example.com')
+        test_user.set_password('Test2024$') 
         db.session.add(test_user)
         db.session.commit()
 
